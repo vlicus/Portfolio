c7d191299156a94e7173639d5efbbee2a9560ca3
Samuel Cobas <samu.cobas.ort@gmail.com>
2025-06-11 18:23:47 +0200

    feat: updated personal projects

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
@@ -6,30 +5,46 @@ import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view">
+    <Card className="project-card-view" style={{ position: "relative" }}>
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
+        <Card.Text style={{ textAlign: "justify", paddingBottom: "25px" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
+        {!props.demoLink ? (
+          <Button
+            variant="primary"
+            href={props.ghLink}
+            target="_blank"
+            style={{ position: "absolute", bottom: 0, right: "40%" }}
+          >
+            <BsGithub style={{ marginBottom: "5px" }} /> Github
+          </Button>
+        ) : (
+          <Button
+            variant="primary"
+            href={props.ghLink}
+            target="_blank"
+            style={{ position: "absolute", bottom: 0, right: "51%" }}
+          >
+            <BsGithub style={{ marginBottom: "5px" }} /> Github
+          </Button>
+        )}
+
         {"\n"}
         {"\n"}
 
         {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
 
-        {!props.isBlog && props.demoLink && (
+        {props.demoLink && (
           <Button
             variant="primary"
             href={props.demoLink}
             target="_blank"
-            style={{ marginLeft: "10px" }}
+            style={{ position: "absolute", bottom: 0, left: "51%", marginTop: "1rem" }}
           >
-            <CgWebsite /> &nbsp;
+            <CgWebsite style={{ marginBottom: "5px" }} /> &nbsp;
             {"Demo"}
           </Button>
         )}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,9 @@
-import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import alquiler_seguro from "../../Assets/Projects/Alquiler_Seguro_Laptop.png";
+import VR_tour from "../../Assets/Projects/VR_tour_Laptop.png";
+import pokefinder from "../../Assets/Projects/Pokefinder_Laptop.png";
 
 function Projects() {
   return (
@@ -17,74 +13,31 @@ function Projects() {
         <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
+        <p style={{ color: "white" }}>Here are a few projects I've worked on recently.</p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              imgPath={pokefinder}
+              title="Pokefinder"
+              description="A mini web app built using JavaScript that enables users to search and view detailed Pokémon data and images. The app loads the full Pokémon list from the PokéAPI and uses Array.filter to implement search functionality. It displays comprehensive profiles with stats (HP, attack, defense, speed, etc.) and front/back images. Data is handled in JSON format, with careful management of asynchronous API calls to deliver a smooth user experience."
+              ghLink="https://github.com/vlicus/proyecto_colaborativo"
+              demoLink="https://vlicus.github.io/proyecto_colaborativo/"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              imgPath={VR_tour}
+              title="LSBU VR Tour"
+              description="The London South Bank University VR Tour is a web-based Virtual Reality application that enables users to explore various LSBU campus facilities through immersive 360° video scenes using a WebXR-compatible headset and controllers. Built with A-Frame and JavaScript, the app features intuitive navigation including head movement, controller interactions, and teleportation between locations such as the Faraday Wing, Borough Road, Perry Library, Keyworth Centre, and a video tour of the Student Life Centre. Served locally via a Python backend, the platform offers an engaging and interactive way to experience the campus remotely using modern VR technology."
+              ghLink="https://github.com/vlicus/LSBU-AR-VR-TOUR"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              imgPath={alquiler_seguro}
+              title="Secure Rent"
+              description="Alquiler Seguro is a full-featured web platform designed to connect property owners with trustworthy tenants through a rental history–based trust system. It offers secure user authentication, detailed property listings, streamlined rental request management, and a mutual rating system to build confidence between landlords and tenants. The platform includes an admin panel for management and features a responsive design for smooth use on mobile devices. Built with a Node.js and Express backend paired with a React frontend, it leverages technologies like MySQL, JWT, bcryptjs, and various React libraries for a robust, user-friendly experience."
+              ghLink="https://github.com/vlicus/PFB"
             />
           </Col>
         </Row>
